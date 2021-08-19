364658b88a7cc810d22315b35e53b72cba55c75f
MauricioAmyr1993 <RM88758@fiap.com.br>
2021-08-19 18:01:55 -0300

    operacaoigualdaderelacionais

---
+++ OperacaoIgualdadeRelacionais/OperadorIgualdadeRelacionais/src/com/java/igualrelac/OperadorIgualdadeRelacionais.java
@@ -0,0 +1,27 @@
+package com.java.igualrelac;
+
+public class OperadorIgualdadeRelacionais {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		boolean maioridade = idade > 18;
+		
+		System.out.println(maioridade);
+		
+		// outros operadores de igualdade:
+		// x == y - x é igual a y
+		// x!= y - x é diferente de y
+
+		// outros operadores relacionais:
+		// x > y - x é maior que y
+		// x < y - x é menor que y
+		// x >= y - x é maior que ou igual a y
+		// x <= y - x é menor que ou igual a y
+		
+		
+		
+
+	}
+
+}
